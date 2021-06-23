cb6391fa3a18d7496ae3d928d6fbdc6324f6cef6
Giulia Montone <gscmonteiro@gmail.com>
2021-06-23 18:38:41 -0300

    adds styling to contacts, adds scroll reveal script

--- main.js
+++ main.js
@@ -39,3 +39,22 @@ const swiper = new Swiper('.swiper-container', {
   mousewhell: true,
   keyboard: true
 })
+
+/* Scroll Reveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+)
