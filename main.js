fca5459b32870e8cde034eb5aedd7cd0e6f4a27d
Giulia Montone <gscmonteiro@gmail.com>
2021-06-24 12:58:37 -0300

    adds back to top button styling and js function, refactors script.js

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ for (const link of links) {
 }
 
 /* change the page header when scrolling */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll is bigger than the header height
     header.classList.add('scroll')
@@ -28,7 +28,7 @@ window.addEventListener('scroll', function () {
     // scroll is smaller than the header height
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials Carousel */
 const swiper = new Swiper('.swiper-container', {
@@ -54,7 +54,25 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/* Back to top */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When scrolling */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
